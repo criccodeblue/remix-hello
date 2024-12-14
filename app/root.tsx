2e4ed25c085dbc6fb83d16ff5cb8d7d3fe2c8661
Nilesh Kumar Gupta <nileshkumar1114@gmail.com>
2024-12-15 00:49:35 +0530

    format

--- app/root.tsx
+++ app/root.tsx
@@ -1,21 +1,31 @@
 import {
   Form,
   Links,
-  Meta, NavLink, Outlet,
+  Meta,
+  NavLink,
+  Outlet,
   Scripts,
-  ScrollRestoration, useLoaderData, useNavigation, useSubmit,
+  ScrollRestoration,
+  useLoaderData,
+  useNavigation,
+  useSubmit,
 } from "@remix-run/react";
 
 import appStyleHref from "./app.css?url";
-import {json, LinksFunction, LoaderFunctionArgs, redirect} from "@remix-run/node";
-import {createEmptyContact, getContacts} from "~/data";
-import {useEffect} from "react";
+import {
+  json,
+  LinksFunction,
+  LoaderFunctionArgs,
+  redirect,
+} from "@remix-run/node";
+import { createEmptyContact, getContacts } from "~/data";
+import { useEffect } from "react";
 
 export const links: LinksFunction = () => {
-  return [{rel: "stylesheet", href: appStyleHref }];
-}
+  return [{ rel: "stylesheet", href: appStyleHref }];
+};
 
-export const loader = async ({request}: LoaderFunctionArgs) => {
+export const loader = async ({ request }: LoaderFunctionArgs) => {
   const url = new URL(request.url);
   const q = url.searchParams.get("q");
   const contacts = await getContacts(q);
@@ -25,20 +35,22 @@ export const loader = async ({request}: LoaderFunctionArgs) => {
 export const action = async () => {
   const contact = await createEmptyContact();
   return redirect(`/contacts/${contact.id}/edit`);
-}
+};
 
 export default function App() {
-  const {contacts, q} = useLoaderData<typeof loader>();
-  const navigation = useNavigation()
+  const { contacts, q } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
   const submit = useSubmit();
-  const searching = navigation.location && new URLSearchParams(navigation.location.search).has("q");
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has("q");
 
   useEffect(() => {
     const searchField = document.getElementById("q");
     if (searchField instanceof HTMLInputElement) {
       searchField.value = q || "";
     }
-  }, [q])
+  }, [q]);
 
   return (
     <html lang="en">
@@ -52,13 +64,15 @@ export default function App() {
         <div id="sidebar">
           <h1>Remix Contacts</h1>
           <div>
-            <Form id="search-form" role="search"
-                  onChange={(event) => {
-                    const isFirstSearch = q === null;
-                    submit(event.currentTarget, {
-                      replace: !isFirstSearch,
-                    })
-                  }}
+            <Form
+              id="search-form"
+              role="search"
+              onChange={(event) => {
+                const isFirstSearch = q === null;
+                submit(event.currentTarget, {
+                  replace: !isFirstSearch,
+                });
+              }}
             >
               <input
                 id="q"
@@ -67,7 +81,7 @@ export default function App() {
                 defaultValue={q || ""}
                 type="search"
                 name="q"
-                className={searching ? "loading": ""}
+                className={searching ? "loading" : ""}
               />
               <div id="search-spinner" aria-hidden hidden={!searching} />
             </Form>
@@ -77,37 +91,36 @@ export default function App() {
           </div>
           <nav>
             <ul>
-                {contacts.map((contact) => {
-                    return (
-                        <li key={contact.id}>
-                            <NavLink
-                                className={({ isActive, isPending }) =>
-                                    isActive
-                                        ? "active"
-                                        : isPending
-                                            ? "pending"
-                                            : ""
-                                }
-                                to={`contacts/${contact.id}`}>
-                                {contact.first || contact.last ? (
-                                    <>
-                                        {contact.first} {contact.last}
-                                    </>
-                                ) : (
-                                    <i>No Name</i>
-                                )}{" "}
-                                {contact.favorite ? (
-                                    <span>★</span>
-                                ) : null}
-                            </NavLink>
-
-                        </li>
-                    )
-                })}
+              {contacts.map((contact) => {
+                return (
+                  <li key={contact.id}>
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive ? "active" : isPending ? "pending" : ""
+                      }
+                      to={`contacts/${contact.id}`}
+                    >
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite ? <span>★</span> : null}
+                    </NavLink>
+                  </li>
+                );
+              })}
             </ul>
           </nav>
         </div>
-        <div id="detail" className={navigation.state === "loading" && !searching ? "loading": ""}>
+        <div
+          id="detail"
+          className={
+            navigation.state === "loading" && !searching ? "loading" : ""
+          }
+        >
           <Outlet />
         </div>
 
